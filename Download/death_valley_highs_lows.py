e63a7fccdaf1e5a0415fcc66032a96ce1d7578a1
Sedrick Thomas <sedrick100thomas@gmail.com>
2020-04-14 17:26:22 -0400

    Weather

--- Download/death_valley_highs_lows.py
+++ Download/death_valley_highs_lows.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-filename = 'data/sitka_weather_2018_simple.csv'
+filename = 'data/death_valley_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -12,11 +12,15 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        high = int(row[5])
-        low = int(row[6])
-        dates.append(current_date)
-        highs.append(high)
-        lows.append(low)
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # Plot the high temperatures
 plt.style.use('seaborn')
@@ -26,7 +30,8 @@ ax.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format the plot
-plt.title("Daily high and low temperatures - 2018", fontsize=24)
+title = "Daily high and low temperatures - 2018\n Death Valley, Ca"
+plt.title(title, fontsize=20)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperatures (F)", fontsize=16)
--- Download/rochester_highs_lows.py
+++ Download/rochester_highs_lows.py
@@ -1,24 +1,28 @@
 import csv
-from datetime import datetime
 
 import matplotlib.pyplot as plt
+from datetime import datetime
 
-filename = 'data/sitka_weather_2018_simple.csv'
+filename = 'data/rochester_weather_full.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    # Get dates and high temperatures from this file.
+    # dates, highs, lows
     dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        high = int(row[5])
-        low = int(row[6])
-        dates.append(current_date)
-        highs.append(high)
-        lows.append(low)
+        try:
+            high = int(row[5])
+            low = int(row[6])
+        except ValueError:
+            print(f"Missing value for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
-# Plot the high temperatures
+# Plot the high and low temperatures
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red', alpha=0.5)
@@ -26,10 +30,11 @@ ax.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format the plot
-plt.title("Daily high and low temperatures - 2018", fontsize=24)
-plt.xlabel('', fontsize=16)
+title = "Daily Highs and Lows in Rochester"
+plt.title(title, fontsize=20)
+plt.xlabel('', fontsize=14)
 fig.autofmt_xdate()
-plt.ylabel("Temperatures (F)", fontsize=16)
+plt.ylabel("Temperatures (F)", fontsize=14)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
 plt.show()
