9ced321ec6a826098ecda013999462f28f90b1e9
Sedrick Thomas <sedrick100thomas@gmail.com>
2020-04-15 15:18:14 -0400

    Earthquake Data

--- Download/eq_explore_data.py
+++ Download/eq_explore_data.py
@@ -12,9 +12,11 @@ for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 
 print(mags[:10])
 print(lons[:5])
